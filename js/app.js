4d10c965deb4e76dd60bc4d0874ca749fc5eaf94
jacob kleiman <jacobakleiman@gmail.com>
2021-04-19 15:25:44 -0700

    footer unsticks on scroll down

--- js/app.js
+++ js/app.js
@@ -1,35 +1,14 @@
-// Interactive Footer
-let didScroll;
-let lastScrollTop = 0;
-const delta = 2;
-let navbarHeight = document.querySelector('footer').offsetHeight;
-
-// Add scroll event listener
-window.addEventListener('scroll', function (event) {
-  didScroll = true;
-});
-
-// Every 250 ms
-// setInterval(function () {
-//   if (didScroll) {
-//     hasScrolled();
-//     didScroll = false;
-//   }
-// }, 250);
-
-function hasScrolled() {
-  let st =
-    (window.pageYOffset || document.scrollTop) - (document.clientTop || 0);
-  // Make sure they scroll more than delta
-  if (Math.abs(lastScrollTop - st) <= delta) return;
-  // If they scrolled down and are past the navbar, add class .footer-down
-  // This is necessary so you never see what is "behind" the navbar.
-  if (st > lastScrollTop && st > navbarHeight) {
-    //   Scroll Down
-    document.querySelector('footer').classList.add('footer-down');
+var footer = document.querySelector('footer');
+var scrollHeight = footer.scrollHeight;
+document.addEventListener('scroll', function (event) {
+  console.log(window.scrollY);
+  if (window.scrollY >= scrollHeight) {
+    console.log('unstick');
+    footer.style.position = 'relative';
+    footer.lastElementChild.classList.remove('hidden');
   } else {
-    //   Scroll Up
-    document.querySelector('footer').classList.remove('footer-down');
+    console.log('stick');
+    footer.style.position = 'fixed';
+    footer.lastElementChild.classList.add('hidden');
   }
-  lastScrollTop = st;
-}
+});
