d78948ff8812e4df9ed1a70e0f13bdd12c195e92
jacob kleiman <jacobakleiman@gmail.com>
2021-04-26 15:00:22 -0700

    fixes anchor tag hrefs

--- js/app.js
+++ js/app.js
@@ -3,9 +3,6 @@ let footer = document.querySelector('footer');
 let innerHeight = window.innerHeight;
 let offsetHeight = document.body.offsetHeight;
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
 // https://stackoverflow.com/questions/9439725/javascript-how-to-detect-if-browser-window-is-scrolled-to-bottom
 
 if (innerHeight + window.scrollY >= offsetHeight) {
@@ -39,33 +36,4 @@ document.querySelector('.mobile__button').addEventListener('click', (e) => {
 // Scroll To Footer
 function scrollToFooter() {
   window.scrollTo(0, offsetHeight);
-}
-
-// Next/Previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  const slides = document.querySelectorAll('.carousel__slide');
-  const dots = document.querySelectorAll('.carousel__navigation-button');
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  slides.forEach((slide) => {
-    slide.style.display = 'none';
-  });
-  dots.forEach((dot) => {
-    dot.classList.remove('active');
-  });
-  slides[slideIndex - 1].style.display = '';
-  dots[slideIndex - 1].classList.add('active');
 }
---
+++ js/carousel.js
@@ -0,0 +1,31 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/Previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  const slides = document.querySelectorAll('.carousel__slide');
+  const dots = document.querySelectorAll('.carousel__navigation-button');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  slides.forEach((slide) => {
+    slide.style.display = 'none';
+  });
+  dots.forEach((dot) => {
+    dot.classList.remove('active');
+  });
+  slides[slideIndex - 1].style.display = '';
+  dots[slideIndex - 1].classList.add('active');
+}
