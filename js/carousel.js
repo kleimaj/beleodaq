83ada2b788572205b603669858fd5c606d1bfc60
jacob kleiman <jacobakleiman@gmail.com>
2021-04-26 15:19:00 -0700

    implements automatic carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,16 +1,19 @@
 // https://www.w3schools.com/howto/howto_js_slideshow.asp
 
 let slideIndex = 1;
+let timeout;
 showSlides(slideIndex);
 
 // Next/Previous controls
 function plusSlides(n) {
   showSlides((slideIndex += n));
+  clearTimeout(timeout);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides((slideIndex = n));
+  clearTimeout(timeout);
 }
 
 function showSlides(n) {
@@ -28,6 +31,8 @@ function showSlides(n) {
   dots.forEach((dot) => {
     dot.classList.remove('active');
   });
-  slides[slideIndex - 1].style.display = '';
+  slides[slideIndex - 1].style.display = 'flex';
   dots[slideIndex - 1].classList.add('active');
+  slideIndex > 3 ? (slideIndex = 1) : slideIndex++;
+  timeout = setTimeout(showSlides, 5000);
 }
