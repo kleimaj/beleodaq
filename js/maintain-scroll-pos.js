17c70c986a60c3f9295b32594dd8e7477c969f4d
jacob kleiman <jacobakleiman@gmail.com>
2021-04-29 16:50:05 -0700

    adds comment to maintain-scroll-pos.js

--- js/maintain-scroll-pos.js
+++ js/maintain-scroll-pos.js
@@ -1,3 +1,4 @@
+// https://stackoverflow.com/questions/17642872/refresh-page-and-keep-scroll-position
 document.addEventListener('DOMContentLoaded', function (event) {
   var scrollpos = localStorage.getItem('scrollpos');
   if (scrollpos) window.scrollTo(0, scrollpos);
