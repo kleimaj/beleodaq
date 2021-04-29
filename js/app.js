61fa7fba7738559c241dcf47fa24353ea8190bfd
jacob kleiman <jacobakleiman@gmail.com>
2021-04-29 15:21:50 -0700

    converts header to fixed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 let footer = document.querySelector('footer');
 
 let innerHeight = window.innerHeight;
-let offsetHeight = document.body.offsetHeight;
+// let offsetHeight = document.body.offsetHeight;
+let offsetHeight = getAbsoluteHeight('body');
 
 // https://stackoverflow.com/questions/9439725/javascript-how-to-detect-if-browser-window-is-scrolled-to-bottom
 
@@ -53,4 +54,17 @@ function scrollBack() {
   footer.lastElementChild.classList.add('hidden');
   document.querySelector('.less').classList.add('hidden');
   document.querySelector('.more').classList.remove('hidden');
+}
+
+// Get Inner and Outer height of element (includes margin)
+// https://stackoverflow.com/questions/10787782/full-height-of-a-html-element-div-including-border-padding-and-margin
+function getAbsoluteHeight(el) {
+  // Get the DOM Node if you pass in a string
+  el = typeof el === 'string' ? document.querySelector(el) : el;
+
+  var styles = window.getComputedStyle(el);
+  var margin =
+    parseFloat(styles['marginTop']) + parseFloat(styles['marginBottom']);
+
+  return Math.ceil(el.offsetHeight + margin);
 }
