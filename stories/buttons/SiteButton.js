1be628a402ca29fb7f98b4f9248a6bcb2c239ce7
jacob kleiman <jacobakleiman@gmail.com>
2021-05-11 11:36:04 -0700

    adds SiteButton story

---
+++ stories/buttons/SiteButton.js
@@ -0,0 +1,23 @@
+import '../../css/index.css';
+
+export const createButton = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  onClick,
+}) => {
+  const btn = document.createElement('a');
+  btn.type = 'button';
+  btn.innerText = label;
+  btn.addEventListener('click', onClick);
+
+  const mode = primary
+    ? 'site-buttons__button p'
+    : 'site-buttons__button button--orange p';
+  btn.className = mode;
+
+  btn.style.backgroundColor = backgroundColor;
+
+  return btn;
+};
