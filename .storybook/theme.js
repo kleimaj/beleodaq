d90e056873375234e0857f331bc0f2bc49e23460
jacob kleiman <jacobakleiman@gmail.com>
2021-05-14 12:30:07 -0700

    adds typography.stories.mdx

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Beleodaq Design System',
+  //   brandUrl: 'https://example.com',
+  brandImage: 'https://jacobkleiman.com/beleodaq/assets/svg/beleodaq-logo.svg',
+});
