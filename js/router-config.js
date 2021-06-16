4a98a865fb1c4fe7be14f897126c083fae79bacb
jacob kleiman <jacobakleiman@gmail.com>
2021-06-16 16:05:18 -0300

    works carousel into ui-view

--- js/router-config.js
+++ js/router-config.js
@@ -1,43 +1,15 @@
 var app = angular.module('BELEODAQ', ['ui.router']);
 
 app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-  //$urlRouterProvider.when("", "/");
+  $urlRouterProvider.when('', '/');
   $urlRouterProvider.otherwise('/');
 
   $locationProvider.html5Mode({
     enabled: true,
   });
 
-  $stateProvider
-    .state('LandingPage', {
-      url: '/',
-      templateUrl: 'carousel.html',
-    })
-    .state('ExhibitHall', {
-      url: '/exhibit-hall',
-      templateUrl: 'exhibit-hall.html',
-      controller: 'hallCtrl',
-    })
-    /*** POD A ***/
-    .state('PodA', {
-      url: '',
-      templateUrl: 'exhibit-hall.html',
-      controller: 'podCtrl',
-    })
-    .state('PodA.Main', {
-      url: '/role-of-mabs',
-      templateUrl: 'content/pod-a.html',
-    })
-    .state('PodA.1', {
-      url: '/mabs-in-infectious-disease',
-      templateUrl: 'content/pod-a-1.html',
-    })
-    .state('PodA.2', {
-      url: '/vaccines-and-mabs',
-      templateUrl: 'content/pod-a-2.html',
-    })
-    .state('PodA.3', {
-      url: '/patient-mab-eligibility-a',
-      templateUrl: 'content/pod-a-3.html',
-    });
+  $stateProvider.state('LandingPage', {
+    url: '/',
+    templateUrl: 'carousel.html',
+  });
 });
